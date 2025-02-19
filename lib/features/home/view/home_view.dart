94b0fd6797ae5e1f56c4422ea155912111fe52e6
Yilmaz Yagiz Dokumaci <dokumaciyagiz@gmail.com>
2025-02-19 16:41:59 +0300

    Implement basic HomeView with TodoList and sample todos

--- lib/features/home/view/home_view.dart
+++ lib/features/home/view/home_view.dart
@@ -1,13 +1,31 @@
 import 'package:flutter/material.dart';
+import 'package:patrol_example_todo/features/home/widgets/todo_list.dart';
+import 'package:patrol_example_todo/models/todo_model.dart';
 
 class HomeView extends StatelessWidget {
   const HomeView({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      body: Center(
-        child: Text('Home View'),
+    return Scaffold(
+      body: Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 16.0, vertical: 64.0),
+        child: Column(
+          children: [
+            Text(
+              'Todo List',
+              style: Theme.of(context).textTheme.titleMedium,
+            ),
+            Flexible(
+              child: TodoList(
+                todos: [
+                  Todo(title: 'Todo 1', description: 'Description 1'),
+                  Todo(title: 'Todo 2', description: 'Description 2'),
+                ],
+              ),
+            ),
+          ],
+        ),
       ),
     );
   }
