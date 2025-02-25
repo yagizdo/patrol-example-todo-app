ce97dd8cb84b05004080eb912e34a45b91079568
Yilmaz Yagiz Dokumaci <dokumaciyagiz@gmail.com>
2025-02-25 11:05:08 +0300

    Refactor HomeView to use TodoCubit and implement dynamic todo loading

--- lib/features/home/view/home_view.dart
+++ lib/features/home/view/home_view.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:patrol_example_todo/features/home/widgets/todo_list.dart';
 import 'package:patrol_example_todo/models/todo_model.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import '../cubit/todo_cubit.dart';
 
 class HomeView extends StatelessWidget {
   const HomeView({super.key});
@@ -8,29 +10,36 @@ class HomeView extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      appBar: AppBar(title: const Text('Todo App')),
+      body: BlocBuilder<TodoCubit, TodoState>(
+        builder: (context, state) {
+          if (state.isLoading) {
+            return const Center(child: CircularProgressIndicator());
+          }
+
+          if (state.error != null) {
+            return Center(child: Text(state.error!));
+          }
+
+          return TodoList(todos: state.todos);
+        },
+      ),
       floatingActionButton: FloatingActionButton(
-        onPressed: () {},
+        onPressed: () => _showAddTodoDialog(context),
         child: const Icon(Icons.add),
       ),
-      body: Padding(
-        padding: const EdgeInsets.symmetric(horizontal: 16.0, vertical: 64.0),
-        child: Column(
-          children: [
-            Text(
-              'Todo List',
-              style: Theme.of(context).textTheme.titleMedium,
-            ),
-            Flexible(
-              child: TodoList(
-                todos: [
-                  Todo(title: 'Todo 1', description: 'Description 1'),
-                  Todo(title: 'Todo 2', description: 'Description 2'),
-                ],
-              ),
-            ),
-          ],
-        ),
-      ),
     );
+  }
+
+  void _showAddTodoDialog(BuildContext context) {
+    // Show dialog to add new todo
+    context.read<TodoCubit>().addTodo(
+          Todo(
+            id: DateTime.now().toString(),
+            title: 'New Todo',
+            description: '',
+            isCompleted: false,
+          ),
+        );
   }
 }
