4025ba8a37752aaa0609cd6337f82ee75cb1a320
Yilmaz Yagiz Dokumaci <dokumaciyagiz@gmail.com>
2025-02-25 10:29:09 +0300

    Add Bloc state management with TodoCubit

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:patrol_example_todo/features/home/view/home_view.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'features/home/cubit/todo_cubit.dart';
 
 void main() {
   runApp(const MyApp());
@@ -10,13 +12,16 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Patrol Example Todo',
-      theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-        useMaterial3: true,
+    return BlocProvider(
+      create: (context) => TodoCubit(),
+      child: MaterialApp(
+        title: 'Patrol Example Todo',
+        theme: ThemeData(
+          colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+          useMaterial3: true,
+        ),
+        home: const HomeView(),
       ),
-      home: const HomeView(),
     );
   }
 }
