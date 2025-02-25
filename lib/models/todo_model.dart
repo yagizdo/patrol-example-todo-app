8567a745bbca519f57880cd7ad7a0d662d487467
Yilmaz Yagiz Dokumaci <dokumaciyagiz@gmail.com>
2025-02-25 10:29:01 +0300

    Enhance Todo model with Equatable, ID, and completion status

--- lib/models/todo_model.dart
+++ lib/models/todo_model.dart
@@ -1,6 +1,32 @@
-class Todo {
+import 'package:equatable/equatable.dart';
+
+class Todo extends Equatable {
+  final String id;
   final String title;
   final String description;
+  final bool isCompleted;
 
-  Todo({required this.title, required this.description});
+  const Todo({
+    required this.id,
+    required this.title,
+    required this.description,
+    this.isCompleted = false,
+  });
+
+  Todo copyWith({
+    String? id,
+    String? title,
+    String? description,
+    bool? isCompleted,
+  }) {
+    return Todo(
+      id: id ?? this.id,
+      title: title ?? this.title,
+      description: description ?? this.description,
+      isCompleted: isCompleted ?? this.isCompleted,
+    );
+  }
+
+  @override
+  List<Object?> get props => [id, title, description, isCompleted];
 }
